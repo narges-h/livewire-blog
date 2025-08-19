dc1b8d32e94b51d4c5b18b81a1c8c832b412f8ff
narges <narges.80.hoseyni@gmail.com>
2025-08-19 20:07:16 +0330

    add database and data

--- app/Livewire/Index/Index.php
+++ app/Livewire/Index/Index.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire\Index;
 
+use App\Models\Article;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $newArticle;
+    public $bestArticle;
+    public function mount(){
+        $this->newArticle = Article::orderBy('id', 'desc')->take(4)->get();
+        $this->bestArticle = Article::where('is_best', 1)->take(4)->get();
+
+
+    }
     public function render()
     {
 //        return view('livewire.index.index');
